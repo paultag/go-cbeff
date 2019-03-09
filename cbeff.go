bbd307cc25080fb1f671989bbb97260e0d5ce1b1
Paul R. Tagliamonte <paultag@gmail.com>
2019-03-09 17:36:35 -0500

    Initial import; start of it working?

--- cbeff.go
+++ cbeff.go
@@ -4,142 +4,92 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
+	"io/ioutil"
+	"os"
 )
 
-type Options uint8
-
-func (o Options) BitSet(bit uint8) bool       { return (uint8(o)&bit == bit) }
-func (o Options) OptionalFieldsPresent() bool { return o.BitSet(0x08) }
-func (o Options) SignatureOrMAC() bool        { return o.BitSet(0x04) }
-func (o Options) Privacy() bool               { return o.BitSet(0x02) }
-func (o Options) Integrity() bool             { return o.BitSet(0x01) }
-
-type OptionalField uint16
-
-func (of OptionalField) String() string {
-	e, ok := FieldValueName[uint16(of)]
-	if !ok {
-		return "unknown"
-	}
-	return e
-}
-
-func (of OptionalField) Fields() []OptionalField {
-	ret := []OptionalField{}
-	for _, field := range AllOptionalFields {
-		fi := uint16(field)
-		if (uint16(of) & fi) == fi {
-			ret = append(ret, field)
-		}
-	}
-	return ret
-}
-
-const (
-	OptionalFieldSubheaderCount         OptionalField = 0x0001
-	OptionalFieldPID                    OptionalField = 0x0002
-	OptionalFieldPatronFormatIdentifier OptionalField = 0x0004
-	OptionalFieldIndex                  OptionalField = 0x0008
-
-	OptionalFieldBiometricCreationDate OptionalField = 0x0010
-	OptionalFieldValidityPeriod        OptionalField = 0x0020
-	OptionalFieldBiometricType         OptionalField = 0x0040
-	OptionalFieldBiometricSubtype      OptionalField = 0x0080
-
-	OptionalFieldCBEFFHeaderVersion  OptionalField = 0x100
-	OptionalFieldPatronHeaderVersion OptionalField = 0x200
-	OptionalFieldBiometricPurpose    OptionalField = 0x400
-	OptionalFieldBiometricDataType   OptionalField = 0x800
-
-	OptionalFieldBiometricDataQuality OptionalField = 0x1000
-	OptionalFieldCreator              OptionalField = 0x2000
-	OptionalFieldChallengeResponse    OptionalField = 0x4000
-	OptionalFieldPayload              OptionalField = 0x8000
-)
-
-func Reverse(e map[string]uint16) map[uint16]string {
-	ret := map[uint16]string{}
-	for k, v := range e {
-		ret[v] = k
-	}
-	return ret
-}
-
-var (
-	AllOptionalFields = []OptionalField{
-		OptionalFieldSubheaderCount, OptionalFieldPID,
-		OptionalFieldPatronFormatIdentifier, OptionalFieldIndex,
-		OptionalFieldBiometricCreationDate, OptionalFieldValidityPeriod,
-		OptionalFieldBiometricType, OptionalFieldBiometricSubtype,
-		OptionalFieldCBEFFHeaderVersion, OptionalFieldPatronHeaderVersion,
-		OptionalFieldBiometricPurpose, OptionalFieldBiometricDataType,
-		OptionalFieldBiometricDataQuality, OptionalFieldCreator,
-		OptionalFieldChallengeResponse, OptionalFieldPayload,
-	}
-
-	FieldNameValue = map[string]uint16{
-		"SubheaderCount":         0x0001,
-		"PID":                    0x0002,
-		"PatronFormatIdentifier": 0x0004,
-		"Index":                  0x0008,
-		"BiometricCreationDate":  0x0010,
-		"ValidityPeriod":         0x0020,
-		"BiometricType":          0x0040,
-		"BiometricSubtype":       0x0080,
-		"CBEFFHeaderVersion":     0x100,
-		"PatronHeaderVersion":    0x200,
-		"BiometricPurpose":       0x400,
-		"BiometricDataType":      0x800,
-		"BiometricDataQuality":   0x1000,
-		"Creator":                0x2000,
-		"ChallengeResponse":      0x4000,
-		"Payload":                0x8000,
-	}
-	FieldValueName map[uint16]string = Reverse(FieldNameValue)
-)
+type Time [8]byte
 
 type Header struct {
-	BDBFormatOwner [2]byte
-	BDBFormatType  [2]byte
-	Options        Options
+	PatronHeaderVersion   uint8
+	SBHSecurityOptions    uint8
+	BDBLength             uint32
+	SBLength              uint16
+	BDBFormatOwner        uint16
+	BDBFormatType         uint16
+	BiometricCreationDate Time
+	ValidityNotBefore     Time
+	ValidityNotAfter      Time
+	BiometricType         [3]byte
+	BiometricDataType     uint8
+	BiometricDataQuality  uint8
+	Creator               [18]byte
+	FASC                  [25]byte
+	Reserved              [4]byte
+}
+
+type FacialHeader struct {
+	FormatID     [4]byte
+	VersionID    [4]byte
+	RecordLength uint32
+	NumberFaces  uint16
+}
+
+type FacialInformation struct {
+	Length                  uint32
+	NumberOfPoints          uint16
+	BiographicalInformation [6]byte
+	Expression              [2]byte
+	Pose                    [3]byte
+	PoseUncertainty         [3]byte
+}
+
+type ImageInformation struct {
+	Type       uint8
+	DataType   uint8
+	Width      uint16
+	Height     uint16
+	ColorSpace uint8
+	SourceType uint8
+	DeviceType uint16
+	Quality    uint16
 }
 
 func Parse(in io.Reader) (*Header, error) {
-	data := Header{}
-	if err := binary.Read(in, binary.LittleEndian, &data); err != nil {
+	h := Header{}
+	if err := binary.Read(in, binary.BigEndian, &h); err != nil {
 		return nil, err
 	}
 
-	if data.Options.OptionalFieldsPresent() {
-		of := OptionalField(0)
-		if err := binary.Read(in, binary.LittleEndian, &of); err != nil {
-			return nil, err
-		}
-		fmt.Printf("%s\n", of.Fields())
+	fh := FacialHeader{}
+	if err := binary.Read(in, binary.BigEndian, &fh); err != nil {
+		return nil, err
 	}
+
+	fi := FacialInformation{}
+	if err := binary.Read(in, binary.BigEndian, &fi); err != nil {
+		return nil, err
+	}
+
+	ii := ImageInformation{}
+	if err := binary.Read(in, binary.BigEndian, &ii); err != nil {
+		return nil, err
+	}
+
+	data, err := ioutil.ReadAll(io.LimitReader(in, int64(fi.Length)))
+	if err != nil {
+		return nil, err
+	}
+
+	fd, err := os.Create("output.bin")
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+	fd.Write(data)
+
+	_ = data
+	_ = fmt.Printf
 
 	return nil, nil
 }
-
-// typedef struct bioapi_bir {
-// BioAPI_BIR_HEADER Header;
-// BioAPI_BIR_BIOMETRIC_DATA_PTR BiometricData; /* length indicated in header */
-// BioAPI_DATA_PTR Signature; /* NULL if no signature; length is inherent in this type */
-// } BioAPI_BIR, *BioAPI_BIR_PTR;
-//
-// typedef struct bioapi_bir_header {
-// uint32 Length; /* Length of Header + Opaque Data */
-// BioAPI_BIR_VERSION HeaderVersion;
-// BioAPI_BIR_DATA_TYPE Type;
-// BioAPI_BIR_BIOMETRIC_DATA_FORMAT Format;
-// BioAPI_QUALITY Quality;
-// BioAPI_BIR_PURPOSE PurposeMask;
-// BioAPI_BIR_AUTH_FACTORS FactorsMask;
-// } BioAPI_BIR_HEADER, *BioAPI_BIR_HEADER_PTR;
-//
-// typedef struct bioapi_bir_biometric_data_format {
-// uint16 FormatOwner;
-// uint16 FormatID;
-// } BioAPI_BIR_BIOMETRIC_DATA_FORMAT, *BioAPI_BIR_BIOMETRIC_DATA_FORMAT_PTR;
-//
-// typedef uint8 BioAPI_BIR_BIOMETRIC_DATA;
--- cmd/cbeff/main.go
+++ cmd/cbeff/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	"encoding/asn1"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"pault.ag/go/cbeff"
@@ -17,9 +20,24 @@ func main() {
 	fd, err := os.Open(os.Args[1])
 	ohshit(err)
 
-	data, err := cbeff.Parse(fd)
+	bytez, err := ioutil.ReadAll(fd)
 	ohshit(err)
 
-	_ = data
+	rv := asn1.RawValue{}
+	rest, err := asn1.Unmarshal(bytez, &rv)
+	ohshit(err)
+
+	if len(rest) != 0 {
+		panic("Trailing garbage")
+	}
+
+	rvN := asn1.RawValue{}
+	_, err = asn1.Unmarshal(rv.Bytes, &rvN)
+	ohshit(err)
+
+	h, err := cbeff.Parse(bytes.NewReader(rvN.Bytes))
+	ohshit(err)
+
+	_ = h
 	_ = fmt.Printf
 }
